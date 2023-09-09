0df63b0edcfbf7d6db62055eb27da93e4e5a560a
Hakim Tokhi <hstokhi@yahoo.com>
2023-09-10 00:38:50 +0200

    Implement First Module for HomePage
    Set up webpack to bundle separate modules into
    the HTML file. Created first module to load
    upon opening of web page. Next step is to create two more
    modules for the site.

---
+++ src/home.js
@@ -0,0 +1,30 @@
+import Sanji from './img/sanji.jpg'
+
+
+const check = () => {
+    console.log('Check 1 2 1 2')
+}
+
+const loadHome = () => {
+    const content = document.querySelector('#content')
+    
+    const name = document.createElement('h1')
+    name.textContent = "Sanji's Cafe"
+    
+    const img = document.createElement('div')
+    img.classList.add('img')
+    const myImage = new Image();
+    myImage.src = Sanji;
+    img.appendChild(myImage)
+    
+
+    const welcome = document.createElement('div')
+    welcome.classList.add('welcome')
+    welcome.textContent = "Welcome to Sanji's Cafe! Come explore the cuisine of the East Blue as we embark on a journey to find Gol D. Roger's Treasure!"
+
+    content.appendChild(name)
+    content.appendChild(img)
+    content.appendChild(welcome)
+}
+
+export {loadHome, check}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
+import './style.css'
+import {loadHome, check} from './home.js'
+
 console.log('Hello World!')
+check()
+loadHome()
